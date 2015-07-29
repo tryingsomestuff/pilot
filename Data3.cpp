bacccd8c3042fffedd9f5b51ae314e9d793817e0
vivien <xr_a_y@yahoo.fr>
2015-07-29 22:26:04 +0200

    ++

--- Data3.cpp
+++ Data3.cpp
@@ -23,13 +23,13 @@ Data3::~Data3(){
 }
 
 double Data3::Toto(){
-	Mediator2 * med = dynamic_cast<Mediator2*>(DataPool::Instance()->GetMediator(TraitMediatorId<Mediator2>::Id()));
+	Mediator2 * med = SafeMediatorAccess<Mediator2>();
 	assert(med!=0);
 	return med->toto;
 }
 
 std::string Data3::Bla() {
-	Mediator1 * med = dynamic_cast<Mediator1*>(DataPool::Instance()->GetMediator(TraitMediatorId<Mediator1>::Id()));
+	Mediator1 * med = SafeMediatorAccess<Mediator1>();
 	assert(med!=0);
 	return med->bla;
 }
